b9313ca487589259171f4d1e1696e390a5ea8930
Bekir Arslan <mail@bekirarslan.info>
2022-01-03 12:43:59 +0300

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,6 @@ const BLOG = {
   sortByDate: false,
   showAbout: true,
   showArchive: true,
-  showBookmarks: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/bkrsln',
