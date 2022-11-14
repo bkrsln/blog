3cdb165435997043d6ee31c288f734607f329875
Bekir Arslan <bekirarslan@outlook.com>
2022-11-14 21:57:06 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -81,7 +81,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
                     width="24"
                     height="24"
                     className="fill-current text-black dark:text-white"
-                    href="favicon.svg"/>
+                    href="favicon.JPG"/>
                   <image width="24" height="24" fill="url(#paint0_radial)" />
                   <defs>
                     <radialGradient
