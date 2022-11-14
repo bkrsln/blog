c05b024f50bffb76c8f96f0b8b2e79db11d0d050
Bekir Arslan <bekirarslan@outlook.com>
2022-11-15 00:09:27 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -84,9 +84,9 @@ class MyDocument extends Document {
               </noscript>
             </>
           )}
-          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon.png"></link>
+          <link rel="icon" href="/favicon.png" type="image/svg+xml" />
+          <link rel="icon" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="192x192" href="/favicon.png"></link>
           <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed"></link>
           
           <link rel="stylesheet" type="text/css" href="https://widgets.superpeer.com/widget.css"></link>
